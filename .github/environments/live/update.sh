84e5d5afc7d5fcc09caa06fe8b837396ec68c38c
Ross Edlin <rossedlin@gmail.com>
2024-02-07 11:43:07 +0000

    Remove migration as we don't need a DB

--- .github/environments/live/update.sh
+++ .github/environments/live/update.sh
@@ -7,5 +7,3 @@ git fetch
 git pull
 
 /opt/alt/php82/usr/bin/php ~/composer.phar install --no-dev --prefer-dist --no-ansi --no-interaction --no-progress
-
-/opt/alt/php82/usr/bin/php artisan migrate:fresh --force
