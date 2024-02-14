f8c45487169fcb0224e3faf8545b171de7468a9f
Ross Edlin <rossedlin@gmail.com>
2024-02-14 08:57:00 +0000

    Add layout.blade.php

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="https://assets.edlin.app/favicon/favicon.ico">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://assets.edlin.app/bootstrap/v5.2/bootstrap.css">
+
+    <!-- Title -->
+    <title>PayPal Laravel</title>
+</head>
+<body>
+<div class="container text-center">
+    <div class="row mt-5">
+        <div class="col-12">
+            <img src="https://assets.edlin.app/logo/codewithross/logo-symbol-dark.png" height='100' alt="Ross Edlin Logo"/>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-12">
+            <h1>PayPal Laravel</h1>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-12">
+            <div class="links">
+                <a href="https://edlin.xyz/website" target="_blank">Home</a>
+                <a href="https://edlin.xyz/portfolio" target="_blank">Portfolio</a>
+                <a href="https://edlin.xyz/contact" target="_blank">Contact</a>
+                <a href="https://edlin.xyz/linkedin" target="_blank">LinkedIn</a>
+                <a href="https://edlin.xyz/github/paypal-laravel" target="_blank">GitHub (Source Code)</a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            @yield('content')
+        </div>
+    </div>
+</div>
+</body>
+</html>
