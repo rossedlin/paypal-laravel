f1ca244c216c0f94dfa53449ac3190518441412e
Ross Edlin <rossedlin@gmail.com>
2024-02-15 15:37:33 +0000

    Remove more Vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,1 @@
 import './bootstrap';
-import {createApp} from 'vue';
-import App from './App.vue';
-import axios from 'axios';
-
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-createApp(App).mount("#app");
